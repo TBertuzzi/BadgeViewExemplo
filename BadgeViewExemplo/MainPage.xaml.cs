56a74554eeb14084035241ea1aa1c660f085eb1c
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-06-19 09:27:31 -0300

    :tada: Exemplo enviado ao GitHub

---
+++ BadgeViewExemplo/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Badge.Plugin;
+using Xamarin.Forms;
+
+namespace BadgeViewExemplo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdicionar_Clicked(object sender, EventArgs e)
+        {
+
+            CrossBadge.Current.SetBadge(105);
+
+        }
+
+        private void btnRemover_Clicked(object sender, EventArgs e)
+        {
+
+            CrossBadge.Current.ClearBadge();
+
+        }
+    }
+}
